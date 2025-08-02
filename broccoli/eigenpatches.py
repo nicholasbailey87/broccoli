4b487a26d48fc1df44b252dfa97cc2112f751ebd
Nicholas Bailey <nicholasbailey@Nicholass-MacBook-Air.local>
2025-08-02 11:12:23 +0100

    Updated eigenpatches.py

--- broccoli/eigenpatches.py
+++ broccoli/eigenpatches.py
@@ -1,14 +1,19 @@
 import torch
 import torch.nn as nn
-from torch.utils.data import DataLoader
 from einops import rearrange
 
 
-def eigenvectors(images: torch.Tensor, patch_size: int = 2, eps=5e-4) -> torch.Tensor:
+def eigenvectors(images: torch.Tensor, patch_size: int, eps=5e-4) -> torch.Tensor:
     """
     Adapted from
-        https://github.com/KellerJordan/cifar10-airbench/blob/master/airbench96_faster.py
+        github.com/KellerJordan/cifar10-airbench/blob/master/airbench96_faster.py
         using https://datascienceplus.com/understanding-the-covariance-matrix/
+
+    Args:
+        images: a batch of training images (the bigger and more representative the better!)
+        patch_size: the size of the eigenvectors we want to create (i.e. the patch/kernel
+            size of the model we will initialise with the eigenvectors)
+        eps: a small number to avoid division by zero
     """
     with torch.no_grad():
         unfolder = nn.Unfold(kernel_size=patch_size, stride=1)
@@ -21,19 +26,3 @@ def eigenvectors(images: torch.Tensor, patch_size: int = 2, eps=5e-4) -> torch.T
         ) / n  # https://datascienceplus.com/understanding-the-covariance-matrix/
         _, eigenvectors = torch.linalg.eigh(covariance_matrix)
         return eigenvectors
-
-
-def get_eigenpatches(data: DataLoader):
-    patches = None
-    for images, _ in data:
-        eigenpatches = eigenvectors(images)
-        if patches is None:
-            patches = eigenpatches
-        else:
-            patches = 0.99 * patches + 0.01 * eigenpatches
-    return patches
-
-
-def plottable_bandpass_filters(eigenpatches: torch.Tensor, h, w, c):
-    bandpass_filters = rearrange(eigenpatches, "N (C H W) -> N H W C", C=3, H=2, W=2)
-    return bandpass_filters.detach().numpy()
