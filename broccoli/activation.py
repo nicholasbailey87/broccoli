d515e56cbc3f831e37ed15af534468b52f8557fd
Nicholas Bailey <nicholasbailey@Nicholass-MacBook-Air.local>
2025-08-14 14:43:03 +0100

    Added ReLU, GELU and SquaredReLU activations.

--- broccoli/activation.py
+++ broccoli/activation.py
@@ -22,3 +22,64 @@ class SwiGLU(nn.Module):
         gate, value = rearrange(x, "... (split c) -> split ... c", split=2)
         beta_swish = gate * F.sigmoid(self.swiglu_beta * gate)
         return beta_swish * value
+
+
+class SquaredReLU(nn.Module):
+    """
+    Squared ReLU, as shown in "ReLU^2 wins" (https://arxiv.org/abs/2402.03804) to
+      be as effective as SwiGLU for training LLMs, possibly because it can allow a
+      NN to learn multyiplication, as noted by
+      https://azizbelaweid.substack.com/p/what-is-swiglu-how-to-implement-it
+    """
+
+    def __init__(self, clamp=True, leaky=True) -> None:
+        super().__init__()
+        self.clamp = clamp
+        self.leaky = leaky
+
+    def forward(self, x):
+        relu_squared = F.relu(x) ** 2
+        if self.clamp:
+            relu_squared = torch.clamp(relu_squared, max=6)
+        if self.leaky:
+            relu_squared = relu_squared + 0.1 * x
+        return relu_squared
+
+
+class ReLU(nn.Module):
+    """
+    A ReLU activation function with optional clamp and leakiness.
+    """
+
+    def __init__(self, clamp=True, leaky=True) -> None:
+        super().__init__()
+        self.clamp = clamp
+        self.leaky = leaky
+
+    def forward(self, x):
+        relu = F.relu(x)
+        if self.clamp:
+            relu = torch.clamp(relu, max=6)
+        if self.leaky:
+            relu = relu + 0.1 * x
+        return relu
+
+
+class GELU(nn.Module):
+    """
+    A ReLU activation function with optional clamp and leakiness.
+    """
+
+    def __init__(self, clamp=True, leaky=True) -> None:
+        super().__init__()
+        self.clamp = clamp
+        self.leaky = leaky
+        self.gelu = nn.GELU()
+
+    def forward(self, x):
+        gelu = self.gelu(x)
+        if self.clamp:
+            gelu = torch.clamp(gelu, max=6)
+        if self.leaky:
+            gelu = gelu + 0.1 * x
+        return gelu
