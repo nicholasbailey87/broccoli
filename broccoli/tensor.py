4cc95ce168739493cdb48e70b117b6237d84929f
Nicholas Bailey <nicholasbailey@Nicholass-MacBook-Air.local>
2025-09-21 15:08:59 +0100

    Renamed the learnable parameters of a sigma reparam tensor to include the pattern  so that they are easily filtered out of weight decay

--- broccoli/tensor.py
+++ broccoli/tensor.py
@@ -14,30 +14,36 @@ class SigmaReparamTensor(nn.Module):
 
         super().__init__()
 
-        self.tensor = nn.Parameter(init_tensor, requires_grad=True)
+        self.sigma_reparam_tensor = nn.Parameter(init_tensor, requires_grad=True)
 
         with torch.no_grad():
-            _, sigma, v_transpose = torch.linalg.svd(self.tensor, full_matrices=False)
+            _, sigma, v_transpose = torch.linalg.svd(
+                self.sigma_reparam_tensor, full_matrices=False
+            )
 
         self.register_buffer("approx_spectral_norm", sigma[:1])
         self.register_buffer("right_singular", v_transpose[0])
-        self.scale = nn.Parameter(
+        self.sigma_reparam_scale = nn.Parameter(
             self.approx_spectral_norm.clone().detach(), requires_grad=True
         )
 
     def power_iteration(self):
         with torch.no_grad():
-            approx_right_singular_transpose = self.tensor.mv(self.right_singular)
+            approx_right_singular_transpose = self.sigma_reparam_tensor.mv(
+                self.right_singular
+            )
             approx_right_singular_transpose = F.normalize(
                 approx_right_singular_transpose, dim=0
             )
-            updated_right_singular = self.tensor.T.mv(approx_right_singular_transpose)
+            updated_right_singular = self.sigma_reparam_tensor.T.mv(
+                approx_right_singular_transpose
+            )
             updated_right_singular = F.normalize(updated_right_singular, dim=0)
             self.right_singular.data.copy_(updated_right_singular)
             rayleigh_quotient = torch.einsum(
                 "m,mn,n->",
                 approx_right_singular_transpose,
-                self.tensor,
+                self.sigma_reparam_tensor,
                 updated_right_singular,
             )
             self.approx_spectral_norm.data.copy_(rayleigh_quotient)
@@ -45,4 +51,6 @@ class SigmaReparamTensor(nn.Module):
     def forward(self):
         if self.training:
             self.power_iteration()
-        return self.scale * (self.tensor / self.approx_spectral_norm)
+        return self.sigma_reparam_scale * (
+            self.sigma_reparam_tensor / self.approx_spectral_norm
+        )
