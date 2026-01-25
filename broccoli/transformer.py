490deefe2c3b9fb5e50a4a25714f7fb12a1084b2
Nicholas Bailey <nicholasbailey@MacBookAir.home>
2026-01-25 09:50:20 +0000

    Fixed a bug where we were still refering to the residual connection within the FF block

--- broccoli/transformer.py
+++ broccoli/transformer.py
@@ -387,13 +387,6 @@ class FeedforwardBlock(nn.Module):
         self.beta = beta
         self.xglu = activation.__name__.endswith("GLU")
 
-        if self.residual_path and (output_features < input_features):
-            raise ValueError(
-                "If the number of output features will be less than "
-                "the number of input features, then `residual_path` "
-                "should be set to False."
-            )
-
         if self.post_norm:
             self.layernorm = nn.LayerNorm(output_features)
 
