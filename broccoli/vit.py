Fixed a bug where I forgot to add the minus sign in the DeepNorm beta definition

broccoli/vit.py
@@ -522,7 +522,7 @@ class ViT(nn.Module):
 
         # Set alpha and beta according to Microsoft's DeepNorm
         self.alpha = (2 * transformer_layers) ** 0.25
-        self.beta = (8 * transformer_layers) ** 0.25
+        self.beta = (8 * transformer_layers) ** -0.25
 
         self.encoder = ViTEncoder(
             input_size=input_size,